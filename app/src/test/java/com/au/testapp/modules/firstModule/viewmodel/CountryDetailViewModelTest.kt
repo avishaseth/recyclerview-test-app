a1b3f35bc29c3c8b10fc6b6c3f622c48b045fc63
avishaseth <sethavisha@gmail.com>
2020-04-18 20:35:29 +1000

    ViewModel TestCase Implementation WIP
    WIP test cases for viewmodel

--- app/src/test/java/com/au/testapp/modules/firstModule/viewmodel/CountryDetailViewModelTest.kt
+++ app/src/test/java/com/au/testapp/modules/firstModule/viewmodel/CountryDetailViewModelTest.kt
@@ -9,6 +9,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import java.lang.reflect.Field
 
 class CountryDetailViewModelTest {
 
@@ -19,13 +20,15 @@ class CountryDetailViewModelTest {
     var rule: TestRule = InstantTaskExecutorRule()
 
     @MockK
-    private lateinit var fetchCountryResult: MutableLiveData<Results>
+    private lateinit var fetchCountryResult:MutableLiveData<Results>
+    var reflection: Field = CountryDetailViewModel::class.java.getDeclaredField("mResults")
 
     @Before
     fun setUp() {
-        MockKAnnotations.init(this, relaxUnitFun = true)
+        MockKAnnotations.init(this, relaxUnitFun = true) // turn relaxUnitFun on for all mocks
         viewModel = spyk(CountryDetailViewModel(),recordPrivateCalls = true)
         every { vmSpy["loadResults"]() } returns fetchCountryResult
+        reflection.isAccessible=true
     }
 
     @Test
@@ -34,11 +37,17 @@ class CountryDetailViewModelTest {
         verify {vmSpy["loadResults"]() }
     }
 
-
     @Test
     fun fetchCountryList_mResultNull_shouldCallLoadResults(){
         vmSpy.fetchCountryList(false)
         verify {vmSpy["loadResults"]() }
     }
 
+    @Test
+    fun fetchCountryList_forceLoadFalse_shouldNeverCallLoadResults(){
+        reflection.set(vmSpy,fetchCountryResult)
+        every { fetchCountryResult.value } returns mockkClass(Results::class)
+        vmSpy.fetchCountryList(false)
+        verify(exactly = 0) {vmSpy["loadResults"]() }
+    }
 }
